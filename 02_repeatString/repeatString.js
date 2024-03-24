f0c4cec5d5df91145260f0a191451842d957873c
Jannine <jannine.osman@gmail.com>
2024-03-24 10:15:17 -0600

    complete repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, number) {
+    let repeatedString = "";
+    if(number < 0)
+    {
+        repeatedString = "ERROR";
+        return repeatedString;
+    }
+    for(let i = 0; i < number; i++)
+    {
+        repeatedString += str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
