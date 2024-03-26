df83455dedc5ec19c557c149f31267d8f8aa1154
Jannine <jannine.osman@gmail.com>
2024-03-25 18:49:32 -0600

    Implement removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...elementsToRemove) {
+    let finalArray = [];
+    for(let i = 0; i < elementsToRemove.length; i++)
+    {
+        let lengthOfArray = inputArray.length;
+        for(let arrayIndex = 0; arrayIndex < lengthOfArray; arrayIndex++)
+        {
+            if(inputArray[arrayIndex] === elementsToRemove[i])
+            {
+                inputArray.splice(arrayIndex, 1);
+                arrayIndex = 0;
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+removeFromArray([1,2,2,3], 2)
+
+
