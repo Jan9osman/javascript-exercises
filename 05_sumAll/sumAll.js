3b8c20fdde721adc09a3b5deaba9857939f679ab
Jannine <jannine.osman@gmail.com>
2024-03-25 18:58:54 -0600

    Implement sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,32 @@
-const sumAll = function() {
-
+const sumAll = function(lowRange, highRange) {
+    if(typeof lowRange != "number" || lowRange != parseInt(lowRange))
+    {
+        return "ERROR";
+    }
+    if(typeof highRange != "number" || highRange != parseInt(highRange))
+    {
+        return "ERROR";
+    }
+    if(highRange < lowRange)
+    {
+        let temp = highRange;
+        highRange = lowRange;
+        lowRange = temp;
+    }
+    if(lowRange < 0 || highRange < 0)
+    {
+        return "ERROR";
+    }
+    let numberCount = highRange - lowRange + 1;
+    let finalSum = 0;
+    for(let i = 0; i < numberCount; i++)
+    {
+        finalSum += i + lowRange;
+    }
+    return finalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+sumAll(1,4);
