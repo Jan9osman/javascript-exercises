60ece584f6c3c5c872434c030e1b13ee179fcd6b
Jannine <jannine.osman@gmail.com>
2024-04-01 18:23:18 -0600

    Implement temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFarenheit) {
+  let tempInCelsuis = (tempInFarenheit - 32) * (5/9);
+  return Math.round(tempInCelsuis * 10) / 10
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsuis) {
+  let tempInFarenheit = (tempInCelsuis * (9/5)) + 32;
+  return Math.round(tempInFarenheit * 10) / 10
 };
 
 // Do not edit below this line
