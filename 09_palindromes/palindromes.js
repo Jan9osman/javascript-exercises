bb243a5ff1e5fd432a7cb49630c4d84fef60dd8b
Jannine <jannine.osman@gmail.com>
2024-04-24 16:36:30 -0600

    Implement palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    var punctuation = /[\.,?! / +/g,' ']/g; 
+    var noPunctuation = str.replace(punctuation, ""); //remove punctuation and spaces
+    let lowRegStr = noPunctuation.toLowerCase();
+
+    let reverseStr = lowRegStr.split('').reverse().join('');
+
+    return reverseStr === lowRegStr; // "amanap
 };
 
 // Do not edit below this line
