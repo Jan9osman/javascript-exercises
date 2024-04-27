d351d722fdcb9656575a5fa45906a81d335a5a33
Jannine <jannine.osman@gmail.com>
2024-04-26 18:42:28 -0600

    Implement fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 0)
+    {
+        return "OOPS"
+    }
+    if(num == 0)
+    {
+        return 0;
+    }
+    if(num == 1 || num == 2)
+    {
+        return 1;
+    }
+    else
+    {
+        let arr = [1, 1, 2];
+        for(let i = 0; i < num; i++)
+        {
+            arr[i + 2] = arr[i] + arr[i + 1];
+        }
+        return arr[num - 1];
+    }
 };
 
 // Do not edit below this line
