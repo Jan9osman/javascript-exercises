2307aded26738853eabdbd3b4779bddf4f637720
Jannine <jannine.osman@gmail.com>
2024-04-26 18:46:15 -0600

    Implement getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrOfBooks) {
+    let arrOfTitles = arrOfBooks.map(book => book.title);
+    return arrOfTitles;
 };
 
 // Do not edit below this line
