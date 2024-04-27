b47e38e8d7f39fade41d36add579069910541d93
Jannine <jannine.osman@gmail.com>
2024-04-26 19:05:07 -0600

    Implement find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    for(let j = 0; j < people.length; j++)
+    {
+        if(people[j].yearOfDeath === undefined)
+        {
+            people[j].yearOfDeath = new Date().getFullYear();
+        }
+    }
+    let ages = people.map(person => person.yearOfDeath - person.yearOfBirth);
+    let oldest = 0;
+    let name = "";
+    let index = 0;
+    for(let i = 0; i < ages.length; i++)
+    {
+        if(ages[i] > oldest)
+        {
+            oldest = ages[i];
+            index = i;
+        }
+    }
+    return people[index];
 };
 
 // Do not edit below this line
